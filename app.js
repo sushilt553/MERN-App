d89ebaae2ced061c8be84d90684469f7c9e7ac04
Sushil <sushilt553@gmail.com>
2020-03-14 15:30:37 -0400

    Register/Login complete

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const express = require("express");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 const app = express();
 const db = require("./config/keys").mongoURI;
 
@@ -9,6 +12,12 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Express"));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 

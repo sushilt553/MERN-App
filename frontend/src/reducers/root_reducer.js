a0edcdcff178a8217f83c49325243fb4a2a5bf55
Sushil <sushilt553@gmail.com>
2020-03-15 17:53:18 -0400

    MERN completed

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
+import ErrorsReducer from './errors_reducer';
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
-    session
+    tweets: TweetsReducer,
+    session,
+    errors: ErrorsReducer
 });
 
 export default RootReducer;

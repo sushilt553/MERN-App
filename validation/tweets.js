5303301b8ca9ddfcd9f42309fbf649971cbd1aea
Sushil <sushilt553@gmail.com>
2020-03-14 22:18:14 -0400

    Tweets validation complete

--- validation/tweets.js
+++ validation/tweets.js
@@ -3,4 +3,19 @@ const validText = require('./valid-text');
 
 module.exports = function validateTweetInput(data) {
     let errors = {};
+
+    data.text = validText(data.text) ? data.text : '';
+
+    if (Validator.isEmpty(data.text)) {
+        errors.text = 'Text field required';
+    }
+
+    if (!Validator.isLength(data.text, { min: 5, max: 140 })) {
+        errors.text = 'Tweet must be between 5 and 140 characters'
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
 }
